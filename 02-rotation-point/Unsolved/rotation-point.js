5ddb356f6fa096f7d0ef748c49866344b9e060ce
LilTzar <eeoerkeedu@gmail.com>
2022-10-13 19:47:37 -0600

    solved rotationpoint very similarly to roman to int, but with a for loop

--- 02-rotation-point/Unsolved/rotation-point.js
+++ 02-rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,16 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
-var rotationPoint = function(words) {};
+var rotationPoint = function (words) {
+	let prevWord = "";
+	let count = 0;
+	for (let i = 0; i < words.length; i++) {
+		console.log(prevWord, words[i]);
+		if (words[i] < prevWord) {
+			count = i;
+			return count;
+		}
+		prevWord = words[i];
+	}
+	console.log(count);
+	return -1;
+};
