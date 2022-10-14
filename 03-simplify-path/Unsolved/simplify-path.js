651bd35fcefa7f229156d122446480ce0cc2ea2d
LilTzar <eeoerkeedu@gmail.com>
2022-10-13 20:29:36 -0600

    haveing trouble with the shortend path algo

--- 03-simplify-path/Unsolved/simplify-path.js
+++ 03-simplify-path/Unsolved/simplify-path.js
@@ -1,3 +1,30 @@
 // Write a function that takes in an absolute file path and simplifies it
 
-var simplifyPath = function(path) {};
+var simplifyPath = function (path) {
+	// let pathChars = path.split("..");
+	// console.log(pathChars);
+	// let pathLength = pathChars.length;
+	// console.log(pathLength);
+	// console.log(pathChars[pathLength - 1]);
+
+	// if (pathChars[pathLength - 1] === "/") {
+	// 	pathChars.pop();
+	// }
+
+	// let shortenedPath = pathChars.join("");
+	// console.log(shortenedPath);
+
+	// return shortenedPath;
+
+	const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+	const cdUP = "..";
+	const cdOVER = "./";
+	pathArr = path.split("");
+	let shortPath = "/";
+
+	pathArr.map((char) => {
+		console.log(char);
+	});
+
+	return shortPath;
+};
